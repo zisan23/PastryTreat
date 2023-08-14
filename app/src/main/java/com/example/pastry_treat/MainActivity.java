1efb6459a957a4384c8cd07ec39183c34e13dbe5
faiak <azmainfaiak45@gmail.com>
2023-08-15 00:43:40 +0600

    added Mahibs login page to the code and added page changing intents....but login to signup does not work

--- app/src/main/java/com/example/pastry_treat/MainActivity.java
+++ app/src/main/java/com/example/pastry_treat/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.pastry_treat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button goToLoginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_main); ///zisan rendered signup from here, dont know if i should rermove it or not
+
+
+        goToLoginBtn = findViewById(R.id.goToLoginBtn);
+
+        goToLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/pastry_treat/SignUpActivity.java
+++ app/src/main/java/com/example/pastry_treat/SignUpActivity.java
@@ -1,6 +1,20 @@
 package com.example.pastry_treat;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
-public class SignUpActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        //zisan eta main activity te dise.....ami eta ekhane ansi
+
+
+    }
 }
