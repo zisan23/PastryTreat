5eaebfda5b4d1af6c59d27f6eeec1d8de44cfbfc
zisan23 <zisanmahmud12002060@gmail.com>
2023-08-15 10:36:39 +0600

    Linked up signup page -> Adding signup activity, some sound track on signup page

--- app/src/main/java/com/example/pastry_treat/SignUpActivity.java
+++ app/src/main/java/com/example/pastry_treat/SignUpActivity.java
@@ -2,19 +2,64 @@ package com.example.pastry_treat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hbb20.CountryCodePicker;
+
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText email;
+    EditText newpassword;
+    EditText confirmpassword;
+    CountryCodePicker country_code;
+    EditText mobilenumber;
+    Button signupbutton;
+    TextView logintext;
+    MediaPlayer mediaPlayer;
+    public void playsound() {
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+        }
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        //zisan eta main activity te dise.....ami eta ekhane ansi
 
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        newpassword = findViewById(R.id.newpassword);
+        confirmpassword = findViewById(R.id.confirmpassword);
+        country_code = findViewById(R.id.country_code);
+        mobilenumber = findViewById(R.id.mobilenumber);
+        signupbutton = findViewById(R.id.signupbutton);
+        logintext = findViewById(R.id.logintext);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.signupbuttonsound);
+
+        signupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playsound();
+                Toast.makeText(SignUpActivity.this, "Still On Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        logintext.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
