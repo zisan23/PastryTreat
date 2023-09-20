43ae77bd0134eefa7ad4d76d60620a786778769a
Sadman Sakib <ssadman8877@gmail.com>
2023-09-21 00:14:08 +0600

    img modified

--- app/src/main/java/com/example/pastry_treat/HomeActivity.java
+++ app/src/main/java/com/example/pastry_treat/HomeActivity.java
@@ -58,6 +58,8 @@ public class HomeActivity extends AppCompatActivity {
     private ArrayList<HomeRvChildModelClass> top_products;
 
     private ArrayList<HomeRvChildModelClass> you_may_like_it;
+    private ArrayList<HomeRvChildModelClass> popular_products;
+    private ArrayList<HomeRvChildModelClass> best_deals;
 
 
     private ArrayList<HomeRvParentModelClass> homeRvParentModelClassArrayList;
@@ -123,56 +125,59 @@ public class HomeActivity extends AppCompatActivity {
             recyclerView_parent = (RecyclerView) findViewById(R.id.home_rv_parent);
 
             top_products = new ArrayList<>();
-
             you_may_like_it = new ArrayList<>();
+            popular_products = new ArrayList<>();
+            best_deals = new ArrayList<>();
+
 
             homeRvParentModelClassArrayList = new ArrayList<>();
 
 
-            top_products.add(new HomeRvChildModelClass(R.drawable.img1));
-            top_products.add(new HomeRvChildModelClass(R.drawable.img2));
-            top_products.add(new HomeRvChildModelClass(R.drawable.img3));
-            top_products.add(new HomeRvChildModelClass(R.drawable.img4));
-            top_products.add(new HomeRvChildModelClass(R.drawable.img5));
+            top_products.add(new HomeRvChildModelClass(R.drawable.chocobiscuit));
+            top_products.add(new HomeRvChildModelClass(R.drawable.chococake));
+            top_products.add(new HomeRvChildModelClass(R.drawable.waffles));
+            top_products.add(new HomeRvChildModelClass(R.drawable.strawcupcake));
+            top_products.add(new HomeRvChildModelClass(R.drawable.chococupcake));
 
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img3));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img1));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img5));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img3));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
+
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.moosecake));
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.whitecake));
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.jellycake));
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.cheesecake));
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.marbleee));
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.crunchydelight));
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.italianpudding));
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.redforrest));
+            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.blackforrest));
 
 
 
 
             homeRvParentModelClassArrayList.add(new HomeRvParentModelClass("Top Products", top_products));
-            homeRvParentModelClassArrayList.add(new HomeRvParentModelClass("You May Also Like it", you_may_like_it));
+            homeRvParentModelClassArrayList.add(new HomeRvParentModelClass("You May Also Like ", you_may_like_it));
 
-            top_products.clear();
-            you_may_like_it.clear();
+            //top_products.clear();
+            //you_may_like_it.clear();
 
 
-            top_products.add(new HomeRvChildModelClass(R.drawable.img1));
-            top_products.add(new HomeRvChildModelClass(R.drawable.img2));
-            top_products.add(new HomeRvChildModelClass(R.drawable.img3));
-            top_products.add(new HomeRvChildModelClass(R.drawable.img4));
-            top_products.add(new HomeRvChildModelClass(R.drawable.img5));
+            best_deals.add(new HomeRvChildModelClass(R.drawable.strawchocopastry));
+            best_deals.add(new HomeRvChildModelClass(R.drawable.fraisiercake));
+            best_deals.add(new HomeRvChildModelClass(R.drawable.macaroons));
+            best_deals.add(new HomeRvChildModelClass(R.drawable.brownievalentine));
+            best_deals.add(new HomeRvChildModelClass(R.drawable.oreos));
 
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img3));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img1));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img5));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img3));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
-            you_may_like_it.add(new HomeRvChildModelClass(R.drawable.img2));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.dessertkababs));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.donuts));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.creamdelight));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.oreoart));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.bubbleoybillcake));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.yellowicecake));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.cheesecupcake));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.theglobecake));
+            popular_products.add(new HomeRvChildModelClass(R.drawable.chocoberrycake));
 
-            homeRvParentModelClassArrayList.add(new HomeRvParentModelClass("Best of Zisan", top_products));
-            homeRvParentModelClassArrayList.add(new HomeRvParentModelClass("Best of Riya", you_may_like_it));
+            homeRvParentModelClassArrayList.add(new HomeRvParentModelClass("Popular Products", top_products));
+            homeRvParentModelClassArrayList.add(new HomeRvParentModelClass("Best Deals", you_may_like_it));
 
 
 
