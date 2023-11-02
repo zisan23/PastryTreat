9857d54f42593530dd115b5887393bac4fe2810b
faiak <azmainfaiak45@gmail.com>
2023-11-03 01:26:01 +0600

    food items search committed

--- app/src/main/java/com/example/pastry_treat/Fragments/FoodSearchFragment.java
+++ app/src/main/java/com/example/pastry_treat/Fragments/FoodSearchFragment.java
@@ -1,66 +1,201 @@
 package com.example.pastry_treat.Fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.pastry_treat.Adapters.FoodAdapter;
+import com.example.pastry_treat.Models.FoodModel;
+import com.example.pastry_treat.Models.HomeRvRestaurentChildModel;
+import com.example.pastry_treat.Models.HomeRvRestaurentParentModel;
 import com.example.pastry_treat.R;
+import com.example.pastry_treat.RestaurentActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FoodSearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class FoodSearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText searchEditText;
+    private Button searchBtn;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView recyclerView;
+    private ArrayList<FoodModel> foodModels = new ArrayList<>();
+    private ArrayList<HomeRvRestaurentParentModel> homeRvRestaurentParentModelList = new ArrayList<>();
+    private FirebaseFirestore firestore;
 
     public FoodSearchFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FoodSearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FoodSearchFragment newInstance(String param1, String param2) {
-        FoodSearchFragment fragment = new FoodSearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_search, container, false);
+
+        firestore = FirebaseFirestore.getInstance();
+        View view =  inflater.inflate(R.layout.fragment_food_search, container, false);
+
+        searchEditText = view.findViewById(R.id.fragment_food_search_et_search);
+        searchBtn = view.findViewById(R.id.fragment_food_search_btn);
+        recyclerView = view.findViewById(R.id.fragment_food_search_rv_parent);
+
+        recyclerView.setPadding(10,10,10,10);
+
+        ShowFoodItemsRecyclerView();
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = searchEditText.getText().toString().trim();
+
+                if(query == null){
+                    Toast.makeText(getContext(), "Provide a valid food name", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    ShowQueriedFoodItemsRecyclerView(query);
+                    searchEditText.setText("");
+                    searchEditText.requestFocus();
+                }
+            }
+        });
+
+
+
+        return view;
+    }
+
+    private void ShowFoodItemsRecyclerView(){
+        try {
+
+            foodModels.clear();
+
+            CollectionReference restaurantsCollection = firestore.collection("foodItems");
+
+            // Query to retrieve all documents in the collection
+            restaurantsCollection
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @SuppressLint("NotifyDataSetChanged")
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+
+                                FoodModel food = documentSnapshot.toObject(FoodModel.class);
+
+
+                                foodModels.add(food);
+
+                            }
+                            //Log.d("Firestore", "Number of documents retrieved: " + queryDocumentSnapshots.size());
+                            //Toast.makeText(HomeActivity.this, queryDocumentSnapshots.toString(), Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(HomeActivity.this, "Restaurants Loaded", Toast.LENGTH_SHORT).show();
+
+                            if(foodModels.isEmpty()){
+                                Toast.makeText(getContext(), "food-models empty", Toast.LENGTH_LONG).show();
+                            }
+
+
+
+                            FoodAdapter foodAdapter = new FoodAdapter(getContext(),foodModels);
+
+                            recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+                            recyclerView.setAdapter(foodAdapter);
+                            foodAdapter.notifyDataSetChanged();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), "foods not loaded", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+
+
+
+        } catch (Exception e) {
+
+            System.out.println("search food items recyclerview not working");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void ShowQueriedFoodItemsRecyclerView(String query){
+        try {
+
+            foodModels.clear();
+
+            CollectionReference restaurantsCollection = firestore.collection("foodItems");
+
+            // Query to retrieve all documents in the collection
+            restaurantsCollection
+                    .whereEqualTo("name",query)
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @SuppressLint("NotifyDataSetChanged")
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+
+                                FoodModel food = documentSnapshot.toObject(FoodModel.class);
+                                foodModels.add(food);
+
+                            }
+                            //Log.d("Firestore", "Number of documents retrieved: " + queryDocumentSnapshots.size());
+                            //Toast.makeText(HomeActivity.this, queryDocumentSnapshots.toString(), Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(HomeActivity.this, "Restaurants Loaded", Toast.LENGTH_SHORT).show();
+
+                            if(foodModels.isEmpty()){
+                                Toast.makeText(getContext(), "no food item with such name found", Toast.LENGTH_LONG).show();
+                            }
+
+                            FoodAdapter foodAdapter = new FoodAdapter(getContext(),foodModels);
+
+                            recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+                            recyclerView.setAdapter(foodAdapter);
+                            foodAdapter.notifyDataSetChanged();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(), "foods not loaded", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+
+        } catch (Exception e) {
+
+            System.out.println("search food items recyclerview not working");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/pastry_treat/Fragments/RestaurantSearchFragment.java
+++ app/src/main/java/com/example/pastry_treat/Fragments/RestaurantSearchFragment.java
@@ -1,66 +1,244 @@
 package com.example.pastry_treat.Fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.pastry_treat.Adapters.HomeRvRestaurentParentAdapter;
+import com.example.pastry_treat.Models.HomeRvRestaurentChildModel;
+import com.example.pastry_treat.Models.HomeRvRestaurentParentModel;
 import com.example.pastry_treat.R;
+import com.example.pastry_treat.SearchActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RestaurantSearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class RestaurantSearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText searchEditText;
+    private Button searchBtn;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView search_rv_parent;
+    private ArrayList<HomeRvRestaurentChildModel> featuredRestaurents = new ArrayList<>();
+    private ArrayList<HomeRvRestaurentParentModel> homeRvRestaurentParentModelList = new ArrayList<>();
+
 
     public RestaurantSearchFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RestaurantSearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RestaurantSearchFragment newInstance(String param1, String param2) {
-        RestaurantSearchFragment fragment = new RestaurantSearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_search, container, false);
+        View view =  inflater.inflate(R.layout.fragment_restaurant_search, container, false);
+
+        search_rv_parent = view.findViewById(R.id.fragment_rest_search_rv_parent);
+        searchEditText = view.findViewById(R.id.fragment_rest_search_et_search);
+        searchBtn = view.findViewById(R.id.fragment_search_btn);
+
+
+        searchEditText.requestFocus();
+
+        ShowRestaurantsRecyclerView();
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = searchEditText.getText().toString().trim();
+
+                if(query == null){
+                    Toast.makeText(getContext(), "Provide a Valid Restaurant Name", Toast.LENGTH_SHORT).show();
+                    searchEditText.requestFocus();
+                }
+                else{
+                    Toast.makeText(getContext(), "Searching" + query, Toast.LENGTH_SHORT).show();
+                    ShowQueriedRestaurantsRecyclerView(query);
+                    searchEditText.setText("");
+                }
+            }
+        });
+
+
+        return view;
     }
+
+
+    private void ShowRestaurantsRecyclerView(){
+        try {
+
+            // Get a reference to the Firestore collection
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            CollectionReference restaurantsCollection = firestore.collection("restaurants");
+
+            // Query to retrieve all documents in the collection
+            restaurantsCollection.get()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @SuppressLint("NotifyDataSetChanged")
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                String location = documentSnapshot.getString("location");
+                                String ownerId = documentSnapshot.getString("ownerId");
+                                String profileImage = documentSnapshot.getString("profile_image");
+                                String restaurantName = documentSnapshot.getString("restaurentName");
+
+                                //Toast.makeText(HomeActivity.this, restaurantName + " " + profileImage, Toast.LENGTH_SHORT).show();
+
+                                HomeRvRestaurentChildModel restaurent = new HomeRvRestaurentChildModel();
+
+                                restaurent.setName(restaurantName);
+                                restaurent.setAddress(location);
+                                restaurent.setImage(profileImage);
+                                restaurent.setShortDescription("description needs to be added");
+                                restaurent.setOwnerId(ownerId);
+
+                                featuredRestaurents.add(restaurent);
+
+                            }
+                            //Log.d("Firestore", "Number of documents retrieved: " + queryDocumentSnapshots.size());
+                            //Toast.makeText(HomeActivity.this, queryDocumentSnapshots.toString(), Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(HomeActivity.this, "Restaurants Loaded", Toast.LENGTH_SHORT).show();
+
+                            if(featuredRestaurents.isEmpty()){
+                                Toast.makeText(getContext(), "featurerestaurants empty", Toast.LENGTH_LONG).show();
+                            }
+
+
+
+
+                            homeRvRestaurentParentModelList.add(new HomeRvRestaurentParentModel("Featured Restaurents", featuredRestaurents));
+
+                            HomeRvRestaurentParentAdapter homeRvRestaurentParentAdapter = new HomeRvRestaurentParentAdapter(getContext(), homeRvRestaurentParentModelList,true);
+                            search_rv_parent.setLayoutManager(new GridLayoutManager(getContext(), LinearLayoutManager.VERTICAL));
+
+                            search_rv_parent.setAdapter(homeRvRestaurentParentAdapter);
+                            homeRvRestaurentParentAdapter.notifyDataSetChanged();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+//                            Toast.makeText(getContext(), "restaurants not loaded", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+
+
+
+        } catch (Exception e) {
+
+            System.out.println("search restaurant recyclerview not working");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private void ShowQueriedRestaurantsRecyclerView(String query){
+        try {
+
+
+            featuredRestaurents.clear();
+
+            // Get a reference to the Firestore collection
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            CollectionReference restaurantsCollection = firestore.collection("restaurants");
+
+            // Query to retrieve all documents in the collection
+            restaurantsCollection
+                    .whereEqualTo("restaurentName",query)
+                    .get()
+
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @SuppressLint("NotifyDataSetChanged")
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                                String location = documentSnapshot.getString("location");
+                                String ownerId = documentSnapshot.getString("ownerId");
+                                String profileImage = documentSnapshot.getString("profile_image");
+                                String restaurantName = documentSnapshot.getString("restaurentName");
+
+                                //Toast.makeText(HomeActivity.this, restaurantName + " " + profileImage, Toast.LENGTH_SHORT).show();
+
+                                HomeRvRestaurentChildModel restaurent = new HomeRvRestaurentChildModel();
+
+                                restaurent.setName(restaurantName);
+                                restaurent.setAddress(location);
+                                restaurent.setImage(profileImage);
+                                restaurent.setShortDescription("description needs to be added");
+                                restaurent.setOwnerId(ownerId);
+
+                                featuredRestaurents.add(restaurent);
+
+                            }
+                            //Log.d("Firestore", "Number of documents retrieved: " + queryDocumentSnapshots.size());
+                            //Toast.makeText(HomeActivity.this, queryDocumentSnapshots.toString(), Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(HomeActivity.this, "Restaurants Loaded", Toast.LENGTH_SHORT).show();
+
+                            if(featuredRestaurents.isEmpty()){
+                                Toast.makeText(getContext(), "No Restaurant exist with such name ", Toast.LENGTH_LONG).show();
+                                return;
+                            }
+
+
+
+                            homeRvRestaurentParentModelList.clear();
+                            homeRvRestaurentParentModelList.add(new HomeRvRestaurentParentModel("Queried Restaurants", featuredRestaurents));
+
+                            HomeRvRestaurentParentAdapter homeRvRestaurentParentAdapter = new HomeRvRestaurentParentAdapter(getContext(), homeRvRestaurentParentModelList,true);
+                            search_rv_parent.setLayoutManager(new GridLayoutManager(getContext(),LinearLayoutManager.VERTICAL));
+
+                            search_rv_parent.setAdapter(homeRvRestaurentParentAdapter);
+                            homeRvRestaurentParentAdapter.notifyDataSetChanged();
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+//                            Toast.makeText(HomeActivity.this, "restaurants not loaded", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+
+
+
+        } catch (Exception e) {
+
+            System.out.println("search restaurant recyclerview not working");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
 }
